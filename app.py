1885ce313ae9d0acb2d67e7e4ee5cff5f1bee0ad
Pawel Podberezsky <podberezsky.pawel@yandex.by>
2017-12-28 15:45:02 +0300

    Добавление маршрутов, передача различных параметров в запросе, включение отладки с autoreload

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask import Flask
 #create the application object
 app = Flask(__name__)
 
+#error handling
+app.config["DEBUG"] = True
+
 # use the decorator pattern to
 # link the view function to a url
 @app.route("/")
@@ -13,7 +16,35 @@ app = Flask(__name__)
 
 # define the view using a function, wich  returns a string
 def hello_world():
-	return "Hello, World!"
+	return "Hello, World!?!?!?!"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print(value+1)
+	return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print(value+1)
+	return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+	print(value)
+	return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "Hello, {}".format(name)
+	else :
+		return "Not Found", 404
+
+
 
 if __name__ == "__main__":
 	app.run()
